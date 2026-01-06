58dfd6be3e255cff2ec622bfc0b4c8e15a53f388
Ronny237 <161422915+Ronny237@users.noreply.github.com>
2026-01-06 13:13:10 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.title('File uploader')
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader('Choose a file')
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.subheader('Dataframe')
+    st.write(df)
+    col1,col2 = st.columns(2)
+    with col1:
+        fig1 = plt.figure(figsize=(16,16))
+        sns.scatterplot(x = 'EstimatedSalary',y ='Age', hue='Purchased',data=df)
+        st.pyplot(fig1)
+
+    with col2:
+        fig2 = plt.figure(figsize=(16,16))
+        sns.histplot(df.Age)
+        st.pyplot(fig2) 
